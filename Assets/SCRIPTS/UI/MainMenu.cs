088d3b3c83d6a079c666d6cf00af4167fa092fae
the-qmark <egormalyukow@gmail.com>
2021-08-01 20:25:09 +0300

    Add Main Menu

---
+++ Assets/SCRIPTS/UI/MainMenu.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject mainMenuPanel;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void NewGame()
+    {
+        mainMenuPanel.SetActive(false);
+    }
+
+    public void Exit()
+    {
+
+    }
+}
